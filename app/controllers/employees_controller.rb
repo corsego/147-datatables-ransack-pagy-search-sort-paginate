8f4ed9a855cc626a8e71a428cbae714f82daf98b
yshmarov <yshmarov@gmail.com>
2023-11-01 20:54:33 +0100

    148 turbo frames - search, sort, paginate without page refresh

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -3,8 +3,8 @@ class EmployeesController < ApplicationController
 
   def index
     # @employees = Employee.all
-    # @q = Employee.ransack(params[:q])
-    @q = Employee.ransack(params[:query])
+    @q = Employee.ransack(params[:q])
+    # @q = Employee.ransack(params[:query])
     # @employees = @q.result(distinct: true)
     # @employees = @q.result.includes(:company)
     @pagy, @employees = pagy(@q.result.includes(:company), items: 15)
